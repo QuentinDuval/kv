a6e49d23f768eacd92a70f31ad24148ffc4a50dc
Duval Quentin <senmenty@gmail.com>
2016-11-12 22:48:41 +0100

    add the application process

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,3 +1,11 @@
 defmodule KV do
+  use Application
 
+  @doc """
+  Describes what should happen when the application starts.
+  The link to this module is done via "mix.exs"
+  """
+  def start(_type, _args) do
+    KV.Supervisor.start_link
+  end
 end
